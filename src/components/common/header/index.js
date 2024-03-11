6feade5d5a3685c1c1973fee113642fe66c7a72c
Pramod <pramodaacharya622@gmail.com>
2024-03-11 11:18:26 +0530

    logo and loaction search panel updated

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import './header.css'; 
+import { TiLocationOutline } from "react-icons/ti";
+import { FaCaretDown } from "react-icons/fa";
+
+
+
+
 
 const Header = () => {
     return (
@@ -7,12 +13,16 @@ const Header = () => {
             <div className='max-width header'>
                 <img src='https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png' alt='zomato' className='header-logo'/>
 
-                <div className='header-left'>
+                <div className='header-right'>
                     <div className='header-location-search-container'>
                         <div className='location-wrapper'>
                             <div className='location-icon-name'>
-                            <i className="fi fi-rs-marker"></i>
+                                <div className='absulute-center location-icon'>
+                                    <TiLocationOutline/>
+                                </div>
+                                <div>Banglore</div>
                             </div>
+                            <FaCaretDown className='absulte-center '/>
                         </div>
                     </div>
                 </div>
